5cd630000ec0f2bc7646ee3fedaa691d4777e203
hey-anna-wp <106728222+hey-anna@users.noreply.github.com>
2026-02-07 03:15:53 +0900

    feat(be): user 라우터 기본 구조 추가
    - /api/users 라우터 연결
    - 기본 POST 엔드포인트 추가
    - Postman 테스트 정상 동작 확인

--- todo-be/routes/index.js
+++ todo-be/routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const taskApi = require("./task.api");
+const userApi = require("./user.api");
 
 router.use("/tasks", taskApi);
+router.use("/users", userApi);
 
 module.exports = router;
